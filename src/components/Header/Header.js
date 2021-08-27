3677b5ed3eb0d830b67f4a7701ec299156974532
Maria Slp <mariiasmagina@gmail.com>
2021-08-27 22:27:16 +0100

    make adaptive header at promo

---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import './Header.css';
+import logo from '../../images/logo.svg';
+import Button from '../Button/Button';
+
+export default function Header() {
+  return (
+    <div className="header">
+      <img src={logo} alt="logo" className="header__logo" />
+      <div>
+        <Button className="button__header_sign">Sign Up</Button>
+        <Button className="button__header_sign">Sign In</Button>
+      </div>
+    </div>
+  );
+}
--- src/components/Promo/Promo.js
+++ src/components/Promo/Promo.js
@@ -1,11 +1,12 @@
 import './Promo.css';
 import Button from '../Button/Button';
+import Header from '../Header/Header';
 
 export default function Promo() {
   return (
     <>
-      <header />
       <div className="promo">
+        <Header />
         <div className="promo__container">
           <h1 className="promo__title">Final project by student of Frontend engineering faculty.</h1>
         </div>
