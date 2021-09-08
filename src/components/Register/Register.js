5f6d6650347466fd4defb0223faa9bce7ef3c1e0
Maria Slp <mariiasmagina@gmail.com>
2021-09-08 15:14:01 +0100

    add SignForm component

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,31 +1,7 @@
-import './Register.css';
-import logo from '../../images/logo.svg';
-import Button from '../Button/Button';
+import SignForm from '../SignForm/SignForm';
 
 export default function Register() {
   return (
-    <div className="register">
-      <img src={logo} alt="logo" className="register__logo" />
-      <h2 className="register__title">Welcome!</h2>
-      <form>
-        <label className="register__label" htmlFor="name">
-          Name
-          <input id="name" className="register__input" type="text" required />
-        </label>
-        <label className="register__label" htmlFor="email">
-          Email
-          <input id="email" className="register__input" type="email" required />
-        </label>
-        <label className="register__label" htmlFor="password">
-          Password
-          <input id="password" className="register__input" type="password" required />
-        </label>
-        <Button className="register__submit" type="submit">Register</Button>
-      </form>
-      <p className="register__text">
-        Already have an account?
-        <a className="register__link" href="/signin">Login</a>
-      </p>
-    </div>
+    <SignForm title="Welcome!" submit="Register" text="Already have an account?" link="Login" isRegister />
   );
 }
---
+++ src/components/SignForm/SignForm.js
@@ -0,0 +1,53 @@
+import PropTypes from 'prop-types';
+import './SignForm.css';
+import logo from '../../images/logo.svg';
+import Button from '../Button/Button';
+
+export default function SignForm({
+  title, submit, text, link, isRegister,
+}) {
+  return (
+    <div className="sign">
+      <img src={logo} alt="logo" className="sign__logo" />
+      <h2 className="sign__title">{title}</h2>
+      <form>
+        <label className="sign__label" htmlFor="name">
+          Name
+          <input id="name" className="sign__input" type="text" required />
+        </label>
+        <label className="sign__label" htmlFor="email">
+          Email
+          <input id="email" className="sign__input" type="email" required />
+        </label>
+        {isRegister
+          ? (
+            <label className="sign__label" htmlFor="password">
+              Password
+              <input id="password" className="sign__input" type="password" required />
+            </label>
+          ) : ''}
+        <Button className="sign__submit" type="submit">{submit}</Button>
+      </form>
+      <p className="sign__text">
+        {text}
+        <a className="sign__link" href="/signin">{link}</a>
+      </p>
+    </div>
+  );
+}
+
+SignForm.propTypes = {
+  title: PropTypes.string,
+  submit: PropTypes.string,
+  text: PropTypes.string,
+  link: PropTypes.string,
+  isRegister: PropTypes.bool,
+};
+
+SignForm.defaultProps = {
+  title: '',
+  submit: '',
+  text: '',
+  link: '',
+  isRegister: false,
+};
