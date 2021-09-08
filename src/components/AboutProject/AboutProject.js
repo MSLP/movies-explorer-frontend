c61451a80206b76feb496fbc0240cdfd400db154
Maria Slp <mariiasmagina@gmail.com>
2021-09-08 21:53:16 +0100

    add scroll at Main

--- src/components/AboutProject/AboutProject.js
+++ src/components/AboutProject/AboutProject.js
@@ -1,35 +1,36 @@
 import './AboutProject.css';
+import { forwardRef } from 'react';
 import Title from '../Title/Title';
 
-export default function AboutProject() {
-  return (
-    <div className="about">
-      <Title>About</Title>
-      <div className="about__grid">
-        <div>
-          <h2 className="about__title">Final project included 5 milestones</h2>
-          <p className="about__text">
-            Planning, backend development, UI layout, features implementation, and last touches.
-          </p>
-        </div>
-        <div>
-          <h2 className="about__title">Final project took 5 weeks</h2>
-          <p className="about__text">
-            Each milestone had soft and hard deadlines,
-            which had to be met for successful sign off.
-          </p>
-        </div>
+const AboutProject = forwardRef((props, ref) => (
+  <div ref={ref} className="about">
+    <Title>About</Title>
+    <div className="about__grid">
+      <div>
+        <h2 className="about__title">Final project included 5 milestones</h2>
+        <p className="about__text">
+          Planning, backend development, UI layout, features implementation, and last touches.
+        </p>
       </div>
-      <div className="about__timeline">
-        <div>
-          <p className="about__timeline_1">1 week</p>
-          <p className="about__timeline_name">Back-end</p>
-        </div>
-        <div>
-          <p className="about__timeline_4">4 weeks</p>
-          <p className="about__timeline_name">Front-end</p>
-        </div>
+      <div>
+        <h2 className="about__title">Final project took 5 weeks</h2>
+        <p className="about__text">
+          Each milestone had soft and hard deadlines,
+          which had to be met for successful sign off.
+        </p>
       </div>
     </div>
-  );
-}
+    <div className="about__timeline">
+      <div>
+        <p className="about__timeline_1">1 week</p>
+        <p className="about__timeline_name">Back-end</p>
+      </div>
+      <div>
+        <p className="about__timeline_4">4 weeks</p>
+        <p className="about__timeline_name">Front-end</p>
+      </div>
+    </div>
+  </div>
+));
+
+export default AboutProject;
--- src/components/Promo/Promo.js
+++ src/components/Promo/Promo.js
@@ -1,17 +1,13 @@
 import './Promo.css';
 import Header from '../Header/Header';
-import NavTab from '../NavTab/NavTab';
 
 export default function Promo() {
   return (
-    <>
-      <div className="promo">
-        <Header isMain />
-        <div className="promo__container">
-          <h1 className="promo__title">Final project by student of Frontend engineering faculty.</h1>
-        </div>
+    <div className="promo">
+      <Header isMain />
+      <div className="promo__container">
+        <h1 className="promo__title">Final project by student of Frontend engineering faculty.</h1>
       </div>
-      <NavTab />
-    </>
+    </div>
   );
 }
