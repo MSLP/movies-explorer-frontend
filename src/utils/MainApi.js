2fb72b658f11a01fdfd889a8b1c964bc7bf84d73
Maria Slp <mariiasmagina@gmail.com>
2021-09-22 11:35:03 +0100

    add MainApi and MoviesApi

---
+++ src/utils/MainApi.js
@@ -0,0 +1,70 @@
+class MainApi {
+  constructor(options) {
+    this.baseUrl = options.baseUrl;
+    this.headers = options.headers;
+  }
+
+  getSavedMovies() {
+    return fetch(`${this.baseUrl}/movies`, {
+      method: 'GET',
+      headers: this.headers,
+    })
+      .then((res) => {
+        if (res.ok) return res.json();
+        return Promise.reject(new Error(`Error: ${res.status}`));
+      });
+  }
+
+  // поставить лайк
+  saveMovie() {
+    return fetch(`${this.baseUrl}/movies`, {
+      method: 'POST',
+      headers: this.headers,
+    })
+      .then((res) => {
+        if (res.ok) return res.json();
+        return Promise.reject(new Error(`Error: ${res.status}`));
+      });
+  }
+
+  deleteMovie(movieId) {
+    return fetch(`${this.baseUrl}/movies/${movieId}`, {
+      method: 'DELETE',
+      headers: this.headers,
+    })
+      .then((res) => {
+        if (res.ok) return res.json();
+        return Promise.reject(new Error(`Error: ${res.status}`));
+      });
+  }
+
+  getUserInfo() {
+    return fetch(`${this.baseUrl}/users/me`, {
+      method: 'GET',
+      headers: this.headers,
+    })
+      .then((res) => {
+        if (res.ok) return res.json();
+        return Promise.reject(new Error(`Error: ${res.status}`));
+      });
+  }
+
+  changeUserInfo(newInfo) {
+    return fetch(`${this.baseUrl}/users/me`, {
+      method: 'PATCH',
+      headers: this.headers,
+      body: JSON.stringify(newInfo),
+    })
+      .then((res) => {
+        if (res.ok) return res.json();
+        return Promise.reject(new Error(`Error: ${res.status}`));
+      });
+  }
+}
+
+export default new MainApi({
+  baseUrl: 'https://api.explorer.mslp.nomoredomains.monster/',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
