321a974d98932a2bcf4061317b69a69ca9fe5bc1
Maria Slp <mariiasmagina@gmail.com>
2021-09-28 00:29:54 +0100

    add toggle for savedMovies

--- src/components/SavedMovies/SavedMovies.js
+++ src/components/SavedMovies/SavedMovies.js
@@ -8,6 +8,7 @@ import api from '../../utils/MainApi';
 export default function Movies() {
   const [movies, setMovies] = useState([]);
   const [filter, setFilter] = useState('');
+  const [toggle, setToggle] = useState(false);
 
   useEffect(() => {
     api.getSavedMovies()
@@ -18,17 +19,24 @@ export default function Movies() {
   }, []);
 
   useEffect(() => {
-    setMovies(movies.filter((movie) => {
-      if (!filter.length) return true;
+    const stringFilter = movies.filter((movie) => {
+      if (!filter.length) return false;
       const searchableMovieName = movie.nameEN?.toLowerCase();
       return searchableMovieName?.includes(filter.trim().toLowerCase()) || false;
-    }));
-  }, [filter]);
+    });
+    if (toggle) {
+      const toggleFilter = stringFilter.filter((movie) => {
+        const searchableMovieTime = movie.duration;
+        return searchableMovieTime <= 40;
+      });
+      setMovies(toggleFilter);
+    } else setMovies(stringFilter);
+  }, [filter, toggle]);
 
   return (
     <>
       <Header isMovies />
-      <Search handleSearch={(data) => setFilter(data)} />
+      <Search handleSearch={(data) => setFilter(data)} handleToggle={() => setToggle(!toggle)} />
       {movies.length ? <MoviesCardList movies={movies} isSaved /> : <p className="movies__not-found">No results</p>}
       <Footer />
     </>
