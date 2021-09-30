ca623021d6f56d8d7e19c8527e235680087ad76c
Maria Slp <mariiasmagina@gmail.com>
2021-09-30 14:38:35 +0100

    add back button

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,12 +1,14 @@
+import { useHistory } from 'react-router-dom';
 import './NotFound.css';
 import Button from '../Button/Button';
 
 export default function NotFound() {
+  const history = useHistory();
   return (
     <div className="page404">
       <h2 className="page404__title">404</h2>
       <p className="page404__subtitle">Page does not exist</p>
-      <Button className="page404__button">Back</Button>
+      <Button onClick={() => history.push('/')} className="page404__button">Back</Button>
     </div>
   );
 }
